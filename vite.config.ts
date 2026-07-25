605b1e311a61ee9a94efa4eb4b91ecb784887609
Mayank N <mayank.narula@apollo247.org>
2026-07-25 11:42:52 +0530

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AlgoLens/',
+  base: '/',
   resolve: {
     alias: {
       'base': path.resolve(__dirname, './src/base'),
